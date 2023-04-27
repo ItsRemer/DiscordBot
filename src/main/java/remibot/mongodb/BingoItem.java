daf326b058379c33df773b06c77a8d322d6bd56e
Jay <remieditsxd@gmail.com>
2023-04-27 02:08:09 -0600

    Forgot Bingo Item

---
+++ src/main/java/remibot/mongodb/BingoItem.java
@@ -0,0 +1,25 @@
+package remibot.mongodb;
+
+import java.util.List;
+
+public class BingoItem {
+
+    private int itemId;
+    private int amount;
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
